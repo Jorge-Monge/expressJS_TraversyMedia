eea0db3f9bbce84cfd5a70c0dd4182ea5608983d
Jordimonk <jordimonk@yahoo.com>
2020-02-03 22:17:59 -0700

    Middleware functions, and parsing request parameters

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Maria Palu",
+    email: "maria@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Pep Rema",
+    email: "pep@gmail.com"
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const path = require("path");
+const logger = require("./middleware/logger");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com"
-  },
-  {
-    id: 2,
-    name: "Maria Palu",
-    email: "maria@gmail.com"
-  },
-  {
-    id: 1,
-    name: "Pep Rema",
-    email: "pep@gmail.com"
-  }
-];
+const members = require("./Members");
+
+// Init middleware
+app.use(logger);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "Public")));
 
 // Create route
-app.get("api/members", (req, res) => {
-  res.json(members);
+app.get("/api/members", (req, res) => res.json(members));
+
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No member with the ID of ${req.params.id} found` });
+  }
 });
 
 /*
