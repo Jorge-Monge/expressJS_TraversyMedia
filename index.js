b428ea5a9dbfa34d9248fd026c0a7a60bf53b9af
Jordimonk <jordimonk@yahoo.com>
2020-02-08 18:39:29 -0700

    Added Express Handlebars middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,9 +10,22 @@ const members = require("./Members");
 // Init middleware
 app.use(logger);
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 // Body parser (for the response of POST)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Routes for the handlebars middleware
+// Homepage route:
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Member App",
+    members
+  });
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "Public")));
